82ae930051bdabc82594380ada79624d3747d9ff
Antonio Loison <antonio.loison@student-cs.fr>
2020-02-15 17:53:34 +0100

    Change dataset index and reformat code

--- white_box/generate_attacks.py
+++ white_box/generate_attacks.py
@@ -11,6 +11,7 @@ from __future__ import unicode_literals
 import logging
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import tensorflow as tf
@@ -24,11 +25,13 @@ from saliency_map_attack import SaliencyMapMethod
 from cleverhans.utils import other_classes
 from cleverhans.utils_tf import silence, model_argmax
 from cleverhans.serial import load
+
 silence()
 
+
 def generate_attacks(save_path, filepath, set_type, dataset, weighted,
-                   train_start=0, train_end=60000, test_start=0,
-                   test_end=10000):
+                     train_start=0, train_end=60000, test_start=0,
+                     test_end=10000):
     """
     Run evaluation on a saved model
     :param save_path: path where attacks will be saved
@@ -45,7 +48,7 @@ def generate_attacks(save_path, filepath, set_type, dataset, weighted,
     sess = tf.Session()
 
     # Get MNIST test data
-    dataset = dataset(train_start=train_start, train_end=train_end, test_start=test_start, test_end=test_end)
+    dataset = dataset(train_start=0, train_end=60000, test_start=0, test_end=10000)
 
     x_set, y_set = dataset.get_set(set_type)
 
@@ -60,7 +63,7 @@ def generate_attacks(save_path, filepath, set_type, dataset, weighted,
 
     # Define input TF placeholder
     x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols,
-                                        nchannels))
+                                          nchannels))
 
     with sess.as_default():
         model = load(filepath)
@@ -70,8 +73,8 @@ def generate_attacks(save_path, filepath, set_type, dataset, weighted,
     # graph
     jsma = SaliencyMapMethod(model, sess=sess)
     jsma_params = {'theta': 1., 'gamma': 0.3,
-                 'clip_min': 0., 'clip_max': 1.,
-                 'y_target': None, 'weighted': weighted}
+                   'clip_min': 0., 'clip_max': 1.,
+                   'y_target': None, 'weighted': weighted}
     preds = model(x)
     # Loop over the samples we want to perturb into adversarial examples
     for sample_ind in range(first_index, last_index):
@@ -87,7 +90,6 @@ def generate_attacks(save_path, filepath, set_type, dataset, weighted,
 
         # Loop over all target classes
         for target in target_classes:
-
             # This call runs the Jacobian-based saliency map approach
             one_hot_target = np.zeros((1, nb_classes), dtype=np.float32)
             one_hot_target[0, target] = 1
@@ -103,21 +105,19 @@ def generate_attacks(save_path, filepath, set_type, dataset, weighted,
             nb_changed = np.where(adv_x_reshape != test_in_reshape)[0].shape[0]
             percent_perturb = float(nb_changed) / adv_x.reshape(-1).shape[0]
             results['number_' + str(sample_ind) + '_' + str(current_class) + '_to_' + str(target)] = \
-            np.concatenate((adv_x_reshape.reshape(-1),
-                            predictions.reshape(-1),
-                            np.array([nb_changed, percent_perturb, res])))
+                np.concatenate((adv_x_reshape.reshape(-1),
+                                predictions.reshape(-1),
+                                np.array([nb_changed, percent_perturb, res])))
 
         sample_vector = sample.reshape(-1)
         shape1 = sample_vector.shape[0]
         shape2 = results.shape[0]
 
-        results['original_image_' + str(sample_ind)] = np.concatenate((sample.reshape(-1), np.zeros((shape2-shape1,))))
+        results['original_image_' + str(sample_ind)] = np.concatenate(
+            (sample.reshape(-1), np.zeros((shape2 - shape1,))))
 
         if weighted:
             attack_type = 'weighted'
         else:
             attack_type = 'simple'
-        results.to_csv(save_path + '/' + attack_type + '_image_' + str(sample_ind) + '.csv', index = False)
-
-
-
+        results.to_csv(save_path + '/' + attack_type + '_image_' + str(sample_ind) + '.csv', index=False)
