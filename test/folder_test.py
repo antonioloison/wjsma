429019957f54c0c818b177646815d8f52e2d5304
Antonio Loison <antonio.loison@student-cs.fr>
2020-02-16 13:20:52 +0100

    Add direct comparison to mnist dataset

--- test/folder_test.py
+++ test/folder_test.py
@@ -8,29 +8,8 @@ import numpy as np
 
 import os
 
+from cleverhans.dataset import MNIST
 
-def different_images(folder_path):
-    """
-    Verify if all original images of the attacks are different
-    :param folder_path: path of the folder containg the dataframes of the attacks
-    :return: if all different, True else False and place of the difference
-    """
-    main_counter = 0
-    for root, dir, files1 in os.walk(folder_path):
-        for file1 in files1:
-            csv1 = pd.read_csv(folder_path + '\\' + file1)
-            image1 = csv1[original_column(csv1)].values
-            sub_counter = 0
-            for root, dir, files2 in os.walk(folder_path):
-                for file2 in files2:
-                    if sub_counter > main_counter:
-                        csv2 = pd.read_csv(folder_path + '\\' + file2)
-                        image2 = csv2[original_column(csv2)].values
-                        if np.array_equal(image1, image2):
-                            return False, (main_counter, sub_counter)
-                    sub_counter += 1
-            main_counter += 1
-    return True
 
 def original_column(csv):
     """
@@ -42,6 +21,28 @@ def original_column(csv):
     for column in columns:
         if 'original' in column:
             return column
+
+def different_images(folder_path):
+    """
+    Verify if all original images of the attacks are different
+    :param folder_path: path of the folder containg the dataframes of the attacks
+    :return: if all different, True else False and place of the difference
+    """
+    counter = 0
+    image_list = []
+    for root, dir, files1 in os.walk(folder_path):
+        for file1 in files1:
+            if counter % 1000 == 0:
+                print(counter)
+            csv1 = pd.read_csv(folder_path + '\\' + file1)
+            image1 = csv1[original_column(csv1)].values
+            reshaped_image = image1.reshape(-1)
+            tuple_image = tuple(reshaped_image)
+            image_list.append(tuple_image)
+            counter += 1
+    return len(image_list) == len(set(image_list))
+
+# print(different_images(r"D:\nn_robustness\le_net_5_final\targeted\test\mnist_simple_test"))
 
 def same_originals(folder_path_1, folder_path_2):
     """
@@ -59,6 +60,8 @@ def same_originals(folder_path_1, folder_path_2):
             return 'weighted'
     attack_type1, attack_type2 = attack_type(folder_path_1), attack_type(folder_path_2)
     for i in range(min(length1,length2)):
+        if i % 1000 == 0:
+            print(i)
         csv1 = pd.read_csv(folder_path_1 + '\\' + attack_type1 + '_image_' + str(i) + '.csv')
         csv2 = pd.read_csv(folder_path_2 + '\\' + attack_type2 + '_image_' + str(i) + '.csv')
         image1 = csv1[original_column(csv1)]
@@ -96,3 +99,35 @@ def all_the_same(folder_path_1, folder_path_2):
 
 # print(same_originals(r"C:\Users\Antonio\Projects\wjsma\white_box\mnist\mnist_simple_test",
 #                      r"C:\Users\Antonio\Projects\wjsma\white_box\mnist\mnist_2_simple_test"))
+
+def same_originals_to_dataset(folder_path, set_type, dataset):
+    """
+    Test if corespondent images in folder are
+    :param folder_path_1: path of first folder
+    :param folder_path_2: path of second folder
+    :return: True if same originals, False and index where the difference is if immages are different
+    """
+    length = len(next(os.walk(folder_path))[2])
+    if dataset == 'mnist':
+        x_set, y_set = MNIST(train_start=0, train_end=60000, test_start=0, test_end=10000).get_set(set_type)
+        image_size = 784
+    if dataset == 'cifar10':
+        x_set, y_set = CIFAR10(train_start=0, train_end=60000, test_start=0, test_end=10000).get_set(set_type)
+        image_size = 3072
+    def attack_type(path):
+        if 'simple' in path:
+            return 'simple'
+        elif 'weighted' in path:
+            return 'weighted'
+    attack_type = attack_type(folder_path)
+    for i in range(length):
+        if i % 1000 == 0:
+            print(i)
+        csv = pd.read_csv(folder_path + '\\' + attack_type + '_image_' + str(i) + '.csv')
+        image1 = csv[original_column(csv)].values[:image_size]
+        image2 = x_set[i:(i+1)].reshape(-1)
+        if not(np.allclose(image1,image2)):
+            return False, i
+    return True
+
+# print(same_originals_to_dataset(r"D:\nn_robustness\le_net_5_final\targeted\train\mnist_simple_train", 'train', 'mnist'))
