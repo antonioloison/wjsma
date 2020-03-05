deb03cfa1dcccc0f0b9c0dd7de76ec7132130ab4
Youlixx <combey.theo@hotmail.com>
2020-03-05 01:57:04 +0100

    Edited test

--- defense/train_defense.py
+++ defense/train_defense.py
@@ -65,6 +65,13 @@ def model_test(weighted):
     x_train, y_train = mnist.get_set('train')
     x_test, y_test = mnist.get_set('test')
 
+    print("CLEAN SET")
+
+    if weighted:
+        model_testing("mnist_defense_weighted.joblib", x_train, y_train, x_test, y_test)
+    else:
+        model_testing("mnist_defense_simple.joblib", x_train, y_train, x_test, y_test)
+
     if weighted:
         x_add = np.load("defense/augmented/x_weighted.npy")[:20000]
         y_add = np.load("defense/augmented/y_weighted.npy")[:20000]
@@ -74,6 +81,8 @@ def model_test(weighted):
 
     x_train = np.concatenate((x_train, x_add.reshape(x_add.shape + (1,))), axis=0).astype(np.float32)
     y_train = np.concatenate((y_train, y_add), axis=0).astype(np.float32)
+
+    print("FULL SET")
 
     if weighted:
         model_testing("mnist_defense_weighted.joblib", x_train, y_train, x_test, y_test)
