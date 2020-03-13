8249d103e5d42da535a4177c60ee3367b3ccab6f
Loison Antonio <antonio.loison@student-cs.fr>
2020-03-13 15:26:58 +0100

    Delete executable files

--- test_augment.sh
+++
@@ -1,5 +0,0 @@
-echo "Testing dataset augmentation with jsma samples"
-python start.py --job augment --settype test --weighted false
-echo "Testing dataset augmentation with wjsma samples"
-python start.py --job augment --settype test --weighted true
-read -p "Press enter to continue"
--- test_model.sh
+++
@@ -1,9 +0,0 @@
-echo "Testing mnist model training"
-python start.py --job train --dataset cifar10
-echo "Testing cifar10 model training"
-python start.py --job train --dataset cifar10
-echo "Testing mnist model testing"
-python start.py --job test --dataset mnist
-echo "Testing cifar10 model testing"
-python start.py --job test --dataset cifar10
-read -p "Press enter to continue"
