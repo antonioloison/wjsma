70a740771d35b8b2cba8364999e55f85d55f3f00
Youlixx <combey.theo@hotmail.com>
2020-04-28 14:25:58 +0200

    Increased the number of generated samples

--- start_power.py
+++ start_power.py
@@ -6,6 +6,6 @@ model_train()
 powers = [0, 1/10, 1/3, 1/2, 1, 2, 3, 10]
 
 for power in powers:
-    mnist_power_save_attacks(True, power, "test", 0, 100)
+    mnist_power_save_attacks(True, power, "test", 0, 1000)
 
-mnist_power_save_attacks(False, 1, "test", 0, 100)
+mnist_power_save_attacks(False, 1, "test", 0, 1000)
